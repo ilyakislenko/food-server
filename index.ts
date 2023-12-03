0445c013fd1d6cbc88a6444d1739d588fe25a048
Ilusaaxd <jlimonka@mail.ru>
2023-12-03 13:35:18 +0700

    additive git manipulations

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from 'express';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { IDish, IFood, IIngredient, TDishType } from './types';
+
+dotenv.config();
+const dishesUrl = './data/dishes.json';
+const ingredientsUrl = './data/ingredients.json';
+const possibleFilters:TDishType[] = ["primary", "vegeterian", "soup", "dessert", "fish", "salad", "drinks", "snacks", "holiday", "cheap"]
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+app.post('/food', (req: Request<{}, {}, IFood>, res: Response<IDish[]>) => {
+  const filters: TDishType[] = Array.from(new Set(req.body.filters ? req.body.filters : [])).filter((fil) => possibleFilters.includes(fil))
+  const dishes: IDish[] = JSON.parse(fs.readFileSync(dishesUrl, 'utf8'));
+  const ingredients: IIngredient[] = JSON.parse(fs.readFileSync(ingredientsUrl, 'utf8'));
+  const answer = dishes.filter(dish => filters.every(fil => dish.types.includes(fil)))
+  res.send(answer);
+})
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
---
+++ types.ts
@@ -0,0 +1,20 @@
+export interface IFood {
+    filters?: TDishType[];
+}
+export interface IIngredient {
+    id: string,
+    title: string,
+    cost: string
+}
+interface IDishIngredient {
+    title: string,
+    amount: string
+}
+export type TDishType = "primary" | "vegeterian" | "soup" | "dessert" | "fish" | "salad" | "drinks" | "snacks" | "holiday" | "cheap"
+export interface IDish {
+    id: string,
+    title: string,
+    description: string,
+    types: TDishType[],
+    ingredients: IDishIngredient[]
+}
